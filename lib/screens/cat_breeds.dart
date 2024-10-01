bad5d46916cf76cf79812af3af0d79ed09a92d15
durul dalkanat <adurul17@hotmail.com>
2024-10-01 09:43:18 -0400

    Refactor cat breed page and cat info page components

--- lib/screens/cat_breeds.dart
+++ lib/screens/cat_breeds.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../api/cats_api.dart';
 import '../components/cat_breed_card.dart';
 import '../model/cats.dart';
 import '../provider/cat_data_provider.dart';
@@ -17,76 +18,88 @@ class CatBreedsPage extends StatefulWidget {
 
 class _CatBreedsPageState extends State<CatBreedsPage> {
   @override
-  void initState() {
-    super.initState();
-    // Fetch cat data when the widget is first created
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      context.read<CatDataProvider>().getCatData();
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: RefreshIndicator(
-        onRefresh: () => context.read<CatDataProvider>().getCatData(),
-        child: _buildBody(),
+    final catDataProvider = CatDataProvider(catAPI: CatAPI());
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => catDataProvider..getCatData(),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: RefreshIndicator(
+          onRefresh: catDataProvider.getCatData,
+          child: _buildBody(catDataProvider),
+        ),
       ),
     );
   }
 
   /// This method will build the body of the page based on the state of the app.
-  Widget _buildBody() {
+  Widget _buildBody(CatDataProvider catDataProvider) {
     return Consumer<CatDataProvider>(
-      builder: (context, catDataProvider, child) {
-        if (catDataProvider.isLoading) {
-          return const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircularProgressIndicator(),
-                SizedBox(height: 16),
-                Text('Loading cat breeds...'),
-              ],
-            ),
-          );
-        } else if (catDataProvider.errorMessage != null) {
-          return Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(catDataProvider.errorMessage!,
-                      style: const TextStyle(
-                        color: Colors.red,
-                        fontFamily: 'NotoSansSymbols',
-                      )),
-                  const SizedBox(height: 25),
-                  ElevatedButton(
-                    onPressed: () => catDataProvider.getCatData(),
-                    child: const Text('Retry'),
-                  ),
-                ],
-              ),
-            ),
-          );
-        } else if (catDataProvider.breeds.isEmpty) {
+      builder: (context, provider, child) {
+        if (provider.isLoading) {
+          return _buildLoadingIndicator();
+        } else if (provider.errorMessage != null) {
+          return _buildErrorMessage(provider);
+        } else if (provider.breeds.isEmpty) {
           return const Center(child: Text('No cat breeds available'));
         } else {
-          return ListView.builder(
-            itemCount: catDataProvider.breeds.length,
-            itemBuilder: (context, index) {
-              return BreedCard(
-                breed: catDataProvider.breeds[index],
-                onTap: _navigateToCatInfo,
-              );
-            },
-          );
+          return _buildBreedList(provider.breeds);
         }
+      },
+    );
+  }
+
+  Widget _buildLoadingIndicator() {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(),
+          SizedBox(height: 16),
+          Text('Loading cat breeds...'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildErrorMessage(CatDataProvider provider) {
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(provider.errorMessage!,
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontFamily: 'NotoSansSymbols',
+                )),
+            const SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: provider.getCatData,
+              child: const Text('Retry'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBreedList(List<Breed> breeds) {
+    return ListView.builder(
+      itemCount: breeds.length,
+      itemBuilder: (context, index) {
+        return BreedCard(
+          breed: breeds[index],
+          onTap: _navigateToCatInfo,
+        );
       },
     );
   }
--- lib/screens/cat_info.dart
+++ lib/screens/cat_info.dart
@@ -1,6 +1,9 @@
+// filename: cat_info.dart
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../api/cats_api.dart';
 import '../components/cat_image_widget.dart';
 import '../model/cats.dart';
 import '../provider/cat_data_provider.dart';
@@ -17,75 +20,83 @@ class CatInfo extends StatefulWidget {
 
 class _CatInfoState extends State<CatInfo> {
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      context.read<CatDataProvider>().getCatSpecificData(widget.catId);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.catBreed),
-      ),
-      body: Consumer<CatDataProvider>(
+    return ChangeNotifierProvider(
+      create: (_) =>
+          CatDataProvider(catAPI: CatAPI())..getCatSpecificData(widget.catId),
+      child: Consumer<CatDataProvider>(
         builder: (context, catDataProvider, child) {
-          return getCatContent(catDataProvider);
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.catBreed),
+            ),
+            body: _buildCatContent(catDataProvider),
+          );
         },
       ),
     );
   }
 
-  Widget getCatContent(CatDataProvider catDataProvider) {
+  Widget _buildCatContent(CatDataProvider catDataProvider) {
     if (catDataProvider.isLoading) {
-      return const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircularProgressIndicator(),
-            SizedBox(height: 16),
-            Text('Loading cat information...'),
-          ],
-        ),
-      );
+      return _buildLoadingIndicator();
     } else if (catDataProvider.errorMessage != null) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(catDataProvider.errorMessage!,
-                style: const TextStyle(color: Colors.red)),
-            ElevatedButton(
-              onPressed: () => catDataProvider.getCatSpecificData(widget.catId),
-              child: const Text('Retry'),
-            ),
-          ],
-        ),
-      );
+      return _buildErrorMessage(catDataProvider);
     } else if (catDataProvider.catBreed != null) {
-      return SingleChildScrollView(
-        child: Card(
-          margin: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              buildCatImage(catDataProvider.catBreed!),
-              const SizedBox(height: 10),
-              Text('Height: ${catDataProvider.catBreed?.height ?? 'Unknown'}'),
-              const SizedBox(height: 10),
-              Text('Width: ${catDataProvider.catBreed?.width ?? 'Unknown'}'),
-              const SizedBox(height: 10),
-            ],
-          ),
-        ),
-      );
+      return _buildCatDetails(catDataProvider.catBreed!);
     } else {
       return const Center(child: Text('No data available'));
     }
   }
 
-  Widget buildCatImage(CatBreed catBreed) {
+  Widget _buildLoadingIndicator() {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(),
+          SizedBox(height: 16),
+          Text('Loading cat information...'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildErrorMessage(CatDataProvider catDataProvider) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(catDataProvider.errorMessage!,
+              style: const TextStyle(color: Colors.red)),
+          ElevatedButton(
+            onPressed: () => catDataProvider.getCatSpecificData(widget.catId),
+            child: const Text('Retry'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCatDetails(CatBreed catBreed) {
+    return SingleChildScrollView(
+      child: Card(
+        margin: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            _buildCatImage(catBreed),
+            const SizedBox(height: 10),
+            Text('Height: ${catBreed.height ?? 'Unknown'}'),
+            const SizedBox(height: 10),
+            Text('Width: ${catBreed.width ?? 'Unknown'}'),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCatImage(CatBreed catBreed) {
     return CatImage(imageUrl: catBreed.url, breed: widget.catBreed);
   }
 }
